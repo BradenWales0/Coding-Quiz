d4e2d12dccefc2f328d4c4c46d8ebf55d321a72e
braden wales <bra.wales00@gmail.com>
2022-11-23 13:48:47 -0600

    added questions

---
+++ js/questions.js
@@ -0,0 +1,32 @@
+var questions = [
+{
+    question: "Inside wgich HTML element do we put the JavaScript?" ,
+    options:["javascript", "script", "js", "scripting"] ,
+    answers:"script"
+
+} ,
+{
+    question: "How do you write a If statement if i is not equal?" ,
+    options:["<>", "!=", "=" ,"+"] ,
+    answers:"!="
+
+} ,
+{
+    title: "The external Javascript file must contain the script tag" ,
+    options:["true", "false"] ,
+    answers:"false"
+
+} ,
+{
+    title: "Is java and Javascript the same?" ,
+    options:["yes", "no"] ,
+    answers:"no"
+
+} ,
+{
+    title: "How do you assign a value to a variable?" ,
+    options:["*", "=", "X", "!"] ,
+    answers:"="
+
+} ,
+]
