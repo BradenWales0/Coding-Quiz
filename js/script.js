99f7b5f94f54bf2df051d23cf433225411c674e6
braden wales <bra.wales00@gmail.com>
2023-01-12 17:09:11 -0600

    updated javascript

---
+++ js/script.js
@@ -0,0 +1,59 @@
+var time = questions.legnth * 30 ;
+var timerId ;
+var currentQuestionIndex = 0 ;
+
+let question1 = {
+    title: 'Inside which HTML element do we put the JavaScript?' ,
+    choices: ["javascript", "script", "js", "scripting"] ,
+    answer: "script" ,
+} ;
+
+element.addEventListener('click', function(){
+    if (correctAnswer == index) {
+        console.log('Correct Answer') ;
+    } else {
+            console.log('Wrong Answer') ;
+        } ;
+})
+
+let question2 = {
+    title: 'How do you write a If statement if i is not equal?' ,
+    choices: ["<>", "!=", "=" ,"+"] ,
+    answer: "!=" ,
+} ;
+
+element.addEventListener('click', function(){
+    if (correctAnswer == index) {
+        console.log('Correct Answer') ;
+    } else {
+            console.log('Wrong Answer') ;
+        } ;
+})
+
+let question3 = {
+    title: 'The external Javascript file must contain the script tag' ,
+    choices: ["true", "false"] ,
+    answer: "false" ,
+} ;
+
+element.addEventListener('click', function(){
+    if (correctAnswer == index) {
+        console.log('Correct Answer') ;
+    } else {
+            console.log('Wrong Answer') ;
+        } ;
+})
+
+let question4 = {
+    title: 'Is java and Javascript the same?' ,
+    choices: ["yes", "no"] ,
+    answer: "no" ,
+} ;
+
+element.addEventListener('click', function(){
+    if (correctAnswer == index) {
+        console.log('Correct Answer') ;
+    } else {
+            console.log('Wrong Answer') ;
+        } ;
+})
